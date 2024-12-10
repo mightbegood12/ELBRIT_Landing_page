3fec6ca582c1518b5a3c591c2187b7b525b72d0a
mightbegood12 <banunaresh1976@gmail.com>
2024-12-10 12:35:41 +0530

    [FIX] SEO

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,6 +26,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="max-snippet:20, max-image-preview:large" />
         <link rel="icon" href="/icon.png"></link>
         <meta
           name="description"
